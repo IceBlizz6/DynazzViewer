178ab08fb9f83aa23a8eab28146a523ba0f49df9
Christian <chr_414@hotmail.com>
2019-10-24 19:06:18 +0200

    Create controllers package

---
+++ src/dynazzviewer/controllers/ServiceDescriptorController.kt
@@ -0,0 +1,83 @@
+package dynazzviewer.controllers
+
+import dynazzviewer.base.ExtDatabase
+import dynazzviewer.entities.MediaPart
+import dynazzviewer.entities.MediaPartCollection
+import dynazzviewer.entities.MediaUnit
+import dynazzviewer.services.descriptors.DescriptionPart
+import dynazzviewer.services.descriptors.DescriptorApi
+import dynazzviewer.services.descriptors.DescriptionUnit
+import dynazzviewer.services.descriptors.ResultHeader
+import dynazzviewer.storage.ReadOperation
+import dynazzviewer.storage.ReadWriteOperation
+import dynazzviewer.storage.Storage
+
+class ServiceDescriptorController(
+	private val descriptorServices : List<DescriptorApi>,
+	private val listener : UpdateListener,
+	private val storage : Storage
+) {
+	fun queryDescriptors(db : ExtDatabase, name : String) : List<ResultHeader> {
+		return descriptorServices
+			.map { e -> e.querySearch(db, name) }
+			.flatMap { e -> e?.toList() ?: listOf() }
+	}
+	
+	fun queryDescriptor(db : ExtDatabase, code : String) : DescriptionUnit? {
+		for (service in descriptorServices) {
+			val response = service.queryLookup(db, code)
+			if (response != null) {
+				return response
+			}
+		}
+		return null
+	}
+	
+	fun add(description : DescriptionUnit) : Int {
+		val mediaUnitId = insertOrUpdate(description)
+		listener.updateMediaUnit(mediaUnitId, recursive=true)
+		return mediaUnitId
+	}
+	
+	private fun insertOrUpdate(description : DescriptionUnit) : Int {
+		storage.readWrite().use { context ->
+			val match = match(description, context)
+			if (match == null) {
+				val mediaUnit = insert(description, context)
+				return mediaUnit.id
+			} else {
+				update(description, match, context)
+				return match.id
+			}
+		}
+	}
+	
+	private fun match(description : DescriptionUnit, context : ReadOperation) : MediaUnit? {
+		val level1 = listOf( description.uniqueKey )
+		val level2 = description.children.map { e -> e.uniqueKey }
+		val level3 : List<String> = description.children.flatMap { e -> e.episodes }.map { e -> e.uniqueKey }
+		val selectAll : List<String> = listOf( level1, level2, level3 ).flatten().filterNotNull()
+		return context.matchExtKey(selectAll)
+	}
+	
+	private fun insert(description : DescriptionUnit, context : ReadWriteOperation) : MediaUnit {
+		val mediaUnit = description.create(context)
+		context.save(mediaUnit)
+		for (season in description.children) {
+			val mediaPartCollection = season.create(parent = mediaUnit)
+			mediaUnit.children.add(mediaPartCollection)
+			context.save(mediaPartCollection)
+			for (episode in season.episodes) {
+				val mediaPart = episode.create(parent = mediaPartCollection)
+				mediaPartCollection.children.add(mediaPart)
+				context.save(mediaPart)
+			}
+		}
+		return mediaUnit
+	}
+	
+	private fun update(description : DescriptionUnit, mediaUnit : MediaUnit, context : ReadWriteOperation) {
+		description.update(mediaUnit, context)
+		context.save(mediaUnit)
+	}
+}
---
+++ test/ServiceDescriptorControllerTest.kt
@@ -0,0 +1,98 @@
+import base.TestConfiguration
+import base.TestUpdateListener
+import dynazzviewer.controllers.ServiceDescriptorController
+import dynazzviewer.services.descriptors.DescriptionPart
+import dynazzviewer.services.descriptors.DescriptionPartCollection
+import dynazzviewer.services.descriptors.DescriptionUnit
+import dynazzviewer.storage.Storage
+import dynazzviewer.storage.sqlite.SqlLiteStorage
+import org.junit.Assert
+import org.junit.Test
+
+class ServiceDescriptorControllerTest {
+	val storage : Storage = SqlLiteStorage(TestConfiguration())
+	val controller = ServiceDescriptorController(
+		storage = storage,
+		listener = TestUpdateListener(),
+		descriptorServices = listOf()
+	)
+	
+	@Test
+	fun addInsertTest() {
+		val mediaUnitId = controller.add(mock1())
+		storage.read().use { context ->
+			val mediaUnit = context.mediaUnitById(mediaUnitId)
+			Assert.assertEquals("Test Series", mediaUnit.name)
+			Assert.assertEquals(1, mediaUnit.children.count())
+			Assert.assertEquals(1, context.mediaPartCollections().count())
+			Assert.assertEquals(1, mediaUnit.children[0].children.count())
+		}
+	}
+	
+	@Test
+	fun addUpdateTest() {
+		val mediaUnitId = controller.add(mock1())
+		val updatedMediaUnitId = controller.add(mock2())
+		Assert.assertEquals(mediaUnitId, updatedMediaUnitId)
+		
+		storage.read().use { context ->
+			Assert.assertEquals(1, context.mediaUnits().count())
+			Assert.assertEquals(1, context.mediaPartCollections().count())
+			Assert.assertEquals(2, context.mediaParts().count())
+		}
+	}
+	
+	private fun mock1() : DescriptionUnit {
+		val descPart = DescriptionPart(
+			name = "Test",
+			uniqueKey = "uniq1",
+			index = 1,
+			aired = null
+		)
+		val descPartCollection = DescriptionPartCollection(
+			name = "Test",
+			extDatabase = null,
+			extDatabaseCode = null,
+			episodes = listOf(descPart),
+			uniqueKey = "uniq2",
+			sortOrder = null
+		)
+		return DescriptionUnit(
+			name = "Test Series",
+			imageUrls = listOf(),
+			children = listOf(descPartCollection),
+			uniqueKey = "uniq3",
+			tags = listOf("Action", "Comedy")
+		)
+	}
+	
+	private fun mock2() : DescriptionUnit {
+		val descPart1 = DescriptionPart(
+			name = "Test",
+			uniqueKey = "uniq1",
+			index = 1,
+			aired = null
+		)
+		val descPart2 = DescriptionPart(
+			name = "Test",
+			uniqueKey = "uniq4",
+			index = 2,
+			aired = null
+		)
+		val descPartCollection = DescriptionPartCollection(
+			name = "Test",
+			extDatabase = null,
+			extDatabaseCode = null,
+			episodes = listOf(descPart1, descPart2),
+			uniqueKey = "uniq2",
+			sortOrder = null
+		)
+		return DescriptionUnit(
+			name = "Test Series",
+			imageUrls = listOf(),
+			children = listOf(descPartCollection),
+			uniqueKey = "uniq3",
+			tags = listOf("Action", "Comedy")
+		)
+	}
+}
