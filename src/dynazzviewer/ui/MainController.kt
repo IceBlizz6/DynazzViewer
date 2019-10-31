f3bd53609dcc0bd74da55b91533edd018245a91d
Kiryuu45 <lysaker.christian@gmail.com>
2019-10-31 23:46:45 +0100

    Drag drop directories

--- src/dynazzviewer/ui/MainController.kt
+++ src/dynazzviewer/ui/MainController.kt
@@ -1,9 +1,16 @@
 package dynazzviewer.ui
 
+import dynazzviewer.filesystem.FileCache
+import dynazzviewer.filesystem.FileEntryFactory
+import dynazzviewer.filesystem.FileRepository
+import dynazzviewer.filesystem.FileSystemRepository
+import dynazzviewer.filesystem.SystemFileSource
 import dynazzviewer.storage.Storage
 import dynazzviewer.storage.sqlite.SqlLiteStorage
+import dynazzviewer.ui.viewmodels.NodeFactory
 import dynazzviewer.ui.viewmodels.NodeViewModel
 import dynazzviewer.ui.viewmodels.RootNodeViewModel
+import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 import tornadofx.Controller
@@ -20,7 +27,25 @@ class MainController : Controller() {
 
     private val storage: Storage
 
+    private val fileRepository: FileRepository
+
     init {
         storage = SqlLiteStorage(configuration)
+        val fileEntryFactory = FileEntryFactory(configuration, storage)
+        val systemFileSource = SystemFileSource(configuration)
+        val fileCache = FileCache(systemFileSource)
+        fileRepository = FileSystemRepository(fileCache, fileEntryFactory)
+    }
+
+    fun addRootDirectories(directories: List<File>) {
+        for (directory in directories) {
+            addRootDirectory(directory.canonicalPath)
+        }
+    }
+
+    private fun addRootDirectory(path: String) {
+        val factory = NodeFactory(fileRepository)
+        val rootDir = factory.createRootViewModel(path, true)
+        fileSystemRoot.children.add(rootDir)
     }
 }
--- src/dynazzviewer/ui/views/FileSystemView.kt
+++ src/dynazzviewer/ui/views/FileSystemView.kt
@@ -5,9 +5,12 @@ import dynazzviewer.ui.viewmodels.DirectoryViewModel
 import dynazzviewer.ui.viewmodels.NodeViewModel
 import dynazzviewer.ui.viewmodels.UnknownViewModelException
 import dynazzviewer.ui.viewmodels.VideoFileViewModel
+import javafx.geometry.Pos
 import javafx.scene.control.SelectionMode
 import javafx.scene.control.TreeItem
-import javafx.scene.image.Image
+import javafx.scene.input.TransferMode
+import javafx.scene.paint.Color
+import javafx.scene.text.TextAlignment
 import tornadofx.*
 
 class FileSystemView : View() {
@@ -16,11 +19,42 @@ class FileSystemView : View() {
     override val root = treeview<NodeViewModel>(TreeItem(mainViewModel.fileSystemRoot)) {
         isShowRoot = false
         selectionModel.selectionMode = SelectionMode.MULTIPLE
-
+        setOnDragOver {
+            if (it.dragboard.hasFiles() && it.dragboard.files.all { e -> e.isDirectory }) {
+                it.acceptTransferModes(TransferMode.LINK)
+            } else {
+                it.consume()
+            }
+        }
+        setOnDragDropped {
+            if (it.dragboard.hasFiles() && it.dragboard.files.all { e -> e.isDirectory }) {
+                mainViewModel.addRootDirectories(it.dragboard.files)
+            }
+            it.isDropCompleted = true
+            it.consume()
+        }
         cellFormat {
             graphic = hbox {
                 when (it) {
-                    is VideoFileViewModel -> label(it.name)
+                    is VideoFileViewModel -> {
+                        if (it.isViewed) {
+                            circle {
+                                fill = Color.GREEN
+                                radius = 7.0
+                            }
+                        } else {
+                            rectangle {
+                                fill = Color.LIGHTBLUE
+                                width = 10.0
+                                height = 10.0
+                                alignment = Pos.CENTER_LEFT
+                            }
+                        }
+                        label {
+                            text = it.name
+                            textAlignment = TextAlignment.CENTER
+                        }
+                    }
                     is DirectoryViewModel -> label(it.name)
                     else -> throw UnknownViewModelException("Unknown FS node type: $it")
                 }
