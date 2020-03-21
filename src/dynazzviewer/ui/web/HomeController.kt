acc5868106e8b4ab9a3bb37c9a8a2ba0f27e7971
Kiryuu45 <lysaker.christian@gmail.com>
2020-03-22 00:52:24 +0100

    Schema print endpoint

--- src/dynazzviewer/ui/web/HomeController.kt
+++ src/dynazzviewer/ui/web/HomeController.kt
@@ -1,12 +1,29 @@
 package dynazzviewer.ui.web
 
+import graphql.schema.GraphQLSchema
+import graphql.schema.idl.SchemaPrinter
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
 
 @Controller
 class HomeController {
+    @Autowired
+    private lateinit var schema: GraphQLSchema
+
     @GetMapping("/")
     fun index(): String {
         return "index"
+    }
+
+    @ResponseBody
+    @GetMapping(value = ["/schema"], produces = ["text/plain"])
+    fun schemaDisplay(): String? {
+        val printer = SchemaPrinter()
+        return printer.print(schema)
+            .replace("@_mappedOperation(operation : \"__internal__\")", "")
+            .replace("@_mappedType(type : \"__internal__\")", "")
     }
 }
