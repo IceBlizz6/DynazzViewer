816d3bd01ae5ae9e7c112445eef1c4296f238f31
IceBlizz6 <lysaker.christian@gmail.com>
2020-07-14 14:49:41 +0200

    Buefy dep

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,8 +3,11 @@ import App from '@/App.vue'
 import router from '@/router'
 import { RouterHandler } from '@/router'
 import MenuHeader from '@/components/MenuHeader.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.component("MenuHeader", MenuHeader)
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
