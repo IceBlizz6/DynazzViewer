735e6aa555585ff55e2e868e0dc4fba5b0b4d092
Christian <lysaker.christian@gmail.com>
2022-01-02 18:30:38 +0100

    Double quote on import

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import router from '@/router'
-import Oruga from '@oruga-ui/oruga-next'
+import Oruga from "@oruga-ui/oruga-next"
 import "@oruga-ui/oruga-next/dist/oruga-full-vars.css"
 import App from "@/App.vue"
 
